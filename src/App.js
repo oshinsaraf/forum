45dbee62866f7f09d75e307dc6700edaf1607ff6
Oshin Saraf <sarafoshin@gmail.com>
2023-10-07 16:02:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,41 @@
- import React from "react";
- import * as Components from './Components';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Import Router, Switch, and Route
+import * as Components from "./Components";
+import Forum from "./home";
 
- function App() {
-     const [signIn, toggle] = React.useState(true);
-      return(
-          <Components.Container>
-              <Components.SignUpContainer signinIn={signIn}>
-                  <Components.Form>
-                      <Components.Title>Create Account</Components.Title>
-                      <Components.Input type='text' placeholder='Name' />
-                      <Components.Input type='email' placeholder='Email' />
-                      <Components.Input type='password' placeholder='Password' />
-                      <Components.Button>Sign Up</Components.Button>
-                  </Components.Form>
-              </Components.SignUpContainer>
+function App() {
+  const [signIn, toggle] = useState(true);
 
-              <Components.SignInContainer signinIn={signIn}>
-                   <Components.Form>
-                       <Components.Title>Sign in</Components.Title>
-                       <Components.Input type='email' placeholder='Email' />
-                       <Components.Input type='password' placeholder='Password' />
-                       <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
-                       <Components.Button>Sigin In</Components.Button>
-                   </Components.Form>
-              </Components.SignInContainer>
+  return (
+    <Router>
+      <div className={`App ${signIn ? "signin" : "signup"}`}>
+        <video autoPlay loop muted className="background-video">
+          <source src="/background-video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <Components.Container>
+          {signIn ? (
+            <Components.SignInContainer signinIn={signIn}>
+              {/* Rest of your Sign In component */}
+            </Components.SignInContainer>
+          ) : (
+            <Components.SignUpContainer signinIn={signIn}>
+              {/* Rest of your Sign Up component */}
+            </Components.SignUpContainer>
+          )}
+          <Components.OverlayContainer signinIn={signIn}>
+            {/* Rest of your Overlay component */}
+          </Components.OverlayContainer>
+        </Components.Container>
 
-              <Components.OverlayContainer signinIn={signIn}>
-                  <Components.Overlay signinIn={signIn}>
+        <Switch>
+          <Route path="/home">
+            <Forum />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 
-                  <Components.LeftOverlayPanel signinIn={signIn}>
-                      <Components.Title>Welcome Back!</Components.Title>
-                      <Components.Paragraph>
-                          To keep connected with us please login with your personal info
-                      </Components.Paragraph>
-                      <Components.GhostButton onClick={() => toggle(true)}>
-                          Sign In
-                      </Components.GhostButton>
-                      </Components.LeftOverlayPanel>
-
-                      <Components.RightOverlayPanel signinIn={signIn}>
-                        <Components.Title>Hello, Friend!</Components.Title>
-                        <Components.Paragraph>
-                            Enter Your personal details and start journey with us
-                        </Components.Paragraph>
-                            <Components.GhostButton onClick={() => toggle(false)}>
-                                Sigin Up
-                            </Components.GhostButton> 
-                      </Components.RightOverlayPanel>
-  
-                  </Components.Overlay>
-              </Components.OverlayContainer>
-
-          </Components.Container>
-      )
- }
-
- export default App;
+export default App;
